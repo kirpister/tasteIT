d0750f9d72ecbf97c8bfd7e43eaf43870a15936d
Alanko Jenni <s2200726@BC-C02J61EAQ6LR-MacBook-Air.local>
2023-01-12 12:52:38 +0200

    change theme button into a proper switch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import '../App.css';
 import { NavLink } from 'react-router-dom';
 
+import moon from '../assets/moon.png';
+import sun from '../assets/sun.png';
+
 import Nav from './Nav';
 
 const Header = ({switchTheme}) => {
@@ -9,7 +12,16 @@ const Header = ({switchTheme}) => {
  return (
         <header>
             <NavLink to="/"><h2>taste<b>IT</b></h2></NavLink>
-            <button onClick={switchTheme}>Switch Theme</button> 
+            {/* <button onClick={switchTheme}>Switch Theme</button>  */}
+            <div className='mode-toggle'>
+            <input type="checkbox" className="checkbox" id="checkbox" onChange={switchTheme} />
+            <label for="checkbox" className="checkbox-label">
+                <img className='sun' src={sun} alt="sun" />
+                <img className='moon' src={moon} alt="moon" />
+                <span className="ball"></span>
+            </label>
+    
+            </div>
             <Nav />
         </header>
     );
